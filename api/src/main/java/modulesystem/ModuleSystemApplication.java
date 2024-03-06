170735f08d776efc6325050c3f7affaa5fb536f1
juwoong kim <wwong7247@gmail.com>
2024-03-06 13:34:17 +0900

    core 에 대한 api 의존성 설정

--- api/src/main/java/modulesystem/ModuleSystemApplication.java
+++ api/src/main/java/modulesystem/ModuleSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuleSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleSystemApplication.class, args);
 	}
-
 }
